56daf971bb1585812a0bbd735f2c23d62b907e7b
joshuahunt10 <joshua.hunt88@gmail.com>
2019-01-06 18:16:49 -0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: {
+      name: '',
+      last: '',
+      age: '',
+      registered: '',
+    },
+    post: '',
+    responseToPost: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => {
+        this.setState({response: res})
+      })
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/test');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    console.log('submit button hit', e);
+  }
+  
   render() {
     return (
       <div className="App">
@@ -20,6 +53,17 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div>
+          It has been a long time since I wrote react.
+        </div>
+        <div>
+          My name is {this.state.response.name} {this.state.response.last} and I am {this.state.response.age}.  My registration status is: {this.state.response.registered}
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Post to Server: </strong>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }
